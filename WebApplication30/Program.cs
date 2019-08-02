aa784a7842577ea39e3a170de8983ff55378dcd0
黃冠霖 <dolphins1123@gmail.com>
2019-08-02 09:31:21 +0800

    更新註解

--- WebApplication30/Program.cs
+++ WebApplication30/Program.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Logging;
 
 namespace WebApplication30
 {
+    /// <summary>
+    /// demo
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
